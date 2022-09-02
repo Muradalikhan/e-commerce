bb986fd09c74e8e8e3cd13649d84e631d3bc453a
Murad khan <murad.jaddoon@gmail.com>
2022-09-02 22:47:40 +0500

    Create productDetail.js

---
+++ src/screen/productDetail.js
@@ -0,0 +1,15 @@
+import Header from "../components/header";
+import Header2 from "../components/header2";
+import Detail from "../components/detail";
+
+const ProductDetail = () => {
+  return (
+    <>
+      <Header />
+      <Header2 />
+      <Detail />
+    </>
+  );
+};
+
+export default ProductDetail;
