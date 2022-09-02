cdf90d668a59384733d8a04e3ceb520e810089e3
Murad khan <murad.jaddoon@gmail.com>
2022-09-02 22:47:36 +0500

    Create 404page.js

---
+++ src/screen/404page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div>
+      <h1 style={{ padding: "10px" }}>404! Page not found </h1>
+    </div>
+  );
+}
+
+export default ErrorPage;
