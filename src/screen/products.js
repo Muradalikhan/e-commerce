40f77a76342956b507a82087f5a2e80263a09344
Murad khan <murad.jaddoon@gmail.com>
2022-09-03 23:52:48 +0500

    code refactoring

--- src/screen/products.js
+++ src/screen/products.js
@@ -1,20 +1,9 @@
 import React from "react";
 import Card from "../components/Card";
-import Cart from "../components/Cart";
-import Header from "../components/header";
-import { useSelector } from "react-redux";
-import Header2 from "../components/header2";
-import SideBar from "../components/sideBar";
 
 function Products() {
-  const showCartstate = useSelector((state) => state.showCart.value);
-
   return (
     <>
-      <Header />
-      <Header2 />
-      {showCartstate ? <Cart /> : null}
-      <SideBar />
       <Card />
     </>
   );
