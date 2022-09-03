954767f2f48e2f4652bcb84566dff39d8f80ccdb
Murad khan <murad.jaddoon@gmail.com>
2022-09-03 23:53:00 +0500

    code refactoring

--- src/config/router/router.js
+++ src/config/router/router.js
@@ -1,28 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ErrorPage from "../../screen/404page";
+import ProductDetail from "../../screen/productDetail";
+import Products from "../../screen/products";
 
-
-import React from 'react';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import ErrorPage from '../../screen/404page';
-import ProductDetail from '../../screen/productDetail';
-import Products from '../../screen/products';
-
-
-
-
-
-const  RouterApp=()=>{
-    
-    return(
-
-        <Router>
-            <Routes>
-                <Route path='/' element={<Products/>} />
-                <Route path='/detail' element={<ProductDetail/>} />
-                <Route path='*' element={<ErrorPage/>} />
-            </Routes>
-        </Router>
-    )
-    
-}
+const RouterApp = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/detail" element={<ProductDetail />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default RouterApp;
