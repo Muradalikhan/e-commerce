75a233e11a8e06bf410f7e6d087a8c001b993ce9
Murad khan <murad.jaddoon@gmail.com>
2022-09-03 23:52:11 +0500

    import layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import RouterApp from "./config/router/router";
+import MainLayout from "./layout/mainLayout";
 
 function App() {
   return (
     <div className="App">
-      <RouterApp />
+      <MainLayout>
+        <RouterApp />
+      </MainLayout>
     </div>
   );
 }
