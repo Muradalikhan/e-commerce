5c0e17fd4a3d024e1d3019b0f44d5057d53f1e68
Murad khan <murad.jaddoon@gmail.com>
2022-09-05 14:18:54 +0500

    created baskit services in redux

--- src/config/redux/basket.js
+++ src/config/redux/basket.js
@@ -6,15 +6,22 @@ export const baskitSlice = createSlice({
   name: "baskit",
   initialState: { value: intitialStateValue },
   reducers: {
-    baskitItem: (state, action) => {
-      state.value =action.payload;
+    addToBaskit: (state, action) => {
+      state.value = [...state.value, action.payload];
     },
     deleteBaskitItem: (state, action) => {
-     state.value= state.value.filter((item) => item.id !== action.payload.id);
+      state.value = state.value.filter((item) => item.id !== action.payload.id);
+    },
+    editBaskitItem: (state, action) => {
+      const editItem = state.value.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      console.log("editabble item is  ::", state.value[editItem].quantity);
     },
   },
 });
 
-export const { baskitItem, deleteBaskitItem } = baskitSlice.actions;
+export const { addToBaskit, deleteBaskitItem, editBaskitItem } =
+  baskitSlice.actions;
 
 export default baskitSlice.reducer;
